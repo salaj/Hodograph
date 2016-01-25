7c67bad0af682356566818b6c9157c37e8dca5a7
Jakub Sala <evans92@o2.pl>
2016-01-25 20:26:17 +0100

    init

---
+++ Hodograph/MainWindow.xaml.cs
@@ -0,0 +1,258 @@
+﻿using OxyPlot;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+namespace Hodograph
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        private void DispatcherTimerTick(object sender, EventArgs e)
+        {
+            var currentTime = DateTime.Now;
+            var timeDiff = currentTime - startTime - timeDelay;
+            Time = timeDiff.TotalSeconds;
+            deltaTime = Time - lastTime;
+            lastTime = Time;
+
+            calculatePositions();
+
+            positionPoints.Add(new DataPoint(Time, currentX));
+
+            //forward derivative
+            currentV = (nextX - currentX) / deltaTime;
+            velocityPoints.Add(new DataPoint(Time, currentV));
+            phasePoints.Add(new DataPoint(currentX, currentV));
+
+            //forward derivative
+            currentA = (nextNextX - 2 * nextX + currentX) / (deltaTime * deltaTime);
+            accelerationPoints.Add(new DataPoint(Time, currentA));
+            reduceGraphPoints();
+        }
+
+
+        private void adjustAngle(ref double Alpha)
+        {
+            if (Alpha > Math.PI * 2) 
+                Alpha -= Math.PI * 2;
+        }
+
+        private void calculateSinglePosition(double angle)
+        {
+            adjustAngle(ref angle);
+            if (Epsilon0 > 0)
+                LCurrent = L + gaussianRandom.GetNormalGaussian(0, Epsilon0);
+            else
+                LCurrent = L;
+            positionOnRim = new Point(Math.Cos(angle) * R, Math.Sin(angle) * R);
+            positionOnBlock = new Point(Math.Sqrt(LCurrent * LCurrent - positionOnRim.Y * positionOnRim.Y) + positionOnRim.X, 0);
+            if (double.IsNaN(positionOnBlock.X)) positionOnBlock.X = 0;
+        }
+
+        private void calculateCurrentPosition()
+        {
+            var angle = Alpha + Omega * deltaTime;
+            calculateSinglePosition(angle);
+            UpdateSceneObjects(positionOnRim, positionOnBlock);
+            currentX = positionOnBlock.X;
+        }
+
+        private void calculateNextPosition()
+        {
+            var angle = Alpha + 2 * Omega * deltaTime;
+            calculateSinglePosition(angle);
+            nextX = positionOnBlock.X;
+        }
+
+        private void calculateNextNextPosition()
+        {
+            var angle = Alpha + 3 * Omega * deltaTime;
+            calculateSinglePosition(angle);
+            nextNextX = positionOnBlock.X;
+        }
+
+        private void calculatePositions()
+        {
+            calculateCurrentPosition();
+            calculateNextPosition();
+            calculateNextNextPosition();
+            //update angle
+            Alpha = Alpha + Omega * deltaTime;
+        }
+
+        private void reduceGraphPoints()
+        {
+            if (positionPoints.Count > MaxPointsCount)
+                positionPoints.RemoveAt(0);
+            if (velocityPoints.Count > MaxPointsCount)
+                velocityPoints.RemoveAt(0);
+            if (accelerationPoints.Count > MaxPointsCount)
+                accelerationPoints.RemoveAt(0);
+            if (phasePoints.Count > MaxPointsCount)
+                phasePoints.RemoveAt(0);
+        }
+
+        private double Norm(Vector3D v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonsFlags[0]) return;
+            for (int i = 0; i < buttonsFlags.Length; i++)
+                buttonsFlags[i] = false;
+            buttonsFlags[0] = true;
+
+            if (!animationStarted)
+            {
+                //AnimationSpeedSlider.IsEnabled = false;
+                animationStarted = true;
+                startTime = DateTime.Now;
+                dispatcherTimer.Start();
+                return;
+            }
+            timeDelay += DateTime.Now - stopTime;
+            dispatcherTimer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonsFlags[1]) return;
+            for (int i = 0; i < buttonsFlags.Length; i++)
+                buttonsFlags[i] = false;
+            buttonsFlags[1] = true;
+
+            dispatcherTimer.Stop();
+            stopTime = DateTime.Now;
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (buttonsFlags[2]) return;
+            for (int i = 0; i < buttonsFlags.Length; i++)
+                buttonsFlags[i] = false;
+            buttonsFlags[2] = true;
+
+            dispatcherTimer.Stop();
+            animationStarted = false;
+            //AnimationSpeedSlider.IsEnabled = true;
+            AnimationSpeed = 0;
+            ResetParameters();
+            SetupSceneObjects();
+
+            positionPoints.Clear();
+            velocityPoints.Clear();
+            accelerationPoints.Clear();
+            phasePoints.Clear();
+            //currentPhasePoints.Clear();
+
+            ResetAxes();
+        }
+
+        private void ResetAxes()
+        {
+            foreach (var axis in PositionPlot.Axes)
+                axis.InternalAxis.Reset();
+            foreach (var axis in VelocityPlot.Axes)
+                axis.InternalAxis.Reset();
+            foreach (var axis in AccelerationPlot.Axes)
+                axis.InternalAxis.Reset();
+            foreach (var axis in PhasePlot.Axes)
+                axis.InternalAxis.Reset();
+        }
+
+        private void ResetParameters()
+        {
+            timeDelay = new TimeSpan();
+            Time = 0;
+            LCurrent = L;
+            Alpha = StartAlpha;
+            currentX = rimBlockDistance;
+            lastTime = 0;
+            deltaTime = 0;
+        }
+
+        private void UpdateSceneObjects(Point positionOnRim, Point positionOnBlock)
+        {
+            axisY.X1 = rimCenter.X;
+            axisY.X2 = rimCenter.X;
+
+            rim.Width = rim.Height = R * 2;
+            Canvas.SetLeft(rim, rimCenter.X - R);
+            Canvas.SetTop(rim, rimCenter.Y - R);
+
+            verticalSpoke.X1 = rimCenter.X + positionOnRim.X;
+            verticalSpoke.Y1 = rimCenter.Y - positionOnRim.Y;
+            verticalSpoke.X2 = rimCenter.X - positionOnRim.X;
+            verticalSpoke.Y2 = rimCenter.Y + positionOnRim.Y;
+
+            horizontalSpoke.X1 = rimCenter.X - positionOnRim.Y;
+            horizontalSpoke.Y1 = rimCenter.Y - positionOnRim.X;
+            horizontalSpoke.X2 = rimCenter.X + positionOnRim.Y;
+            horizontalSpoke.Y2 = rimCenter.Y + positionOnRim.X;
+
+            Canvas.SetLeft(rimPoint, rimCenter.X + positionOnRim.X - 4);
+            Canvas.SetTop(rimPoint, rimCenter.Y - positionOnRim.Y - 4);
+
+            Canvas.SetLeft(blockPoint, rimCenter.X + positionOnBlock.X - 4);
+            Canvas.SetTop(blockPoint, rimCenter.Y - 4);
+
+            joint.X1 = rimCenter.X + positionOnRim.X;
+            joint.Y1 = rimCenter.Y - positionOnRim.Y;
+            joint.X2 = rimCenter.X + positionOnBlock.X;
+            joint.Y2 = rimCenter.Y;
+
+            block.Width = R * 1.5;
+            block.Height = R * 1;
+            Canvas.SetLeft(block, rimCenter.X + positionOnBlock.X);
+            Canvas.SetTop(block, rimCenter.Y - block.Height / 2);
+        }
+
+        private void SetupSceneObjects()
+        {
+            axisX.X1 = 0;
+            axisX.Y1 = rimCenter.Y;
+            axisX.X2 = CanvasWidth;
+            axisX.Y2 = rimCenter.Y;
+
+            axisY.X1 = rimCenter.X;
+            axisY.Y1 = -1000;
+            axisY.X2 = rimCenter.X;
+            axisY.Y2 = 1000;//CanvasHeight;
+
+            rim.Width = rim.Height = R * 2;
+            Canvas.SetLeft(rim, rimCenter.X - R);
+            Canvas.SetTop(rim, rimCenter.Y - R);
+
+            verticalSpoke.X1 = rimCenter.X;
+            verticalSpoke.Y1 = rimCenter.Y - R;
+            verticalSpoke.X2 = rimCenter.X;
+            verticalSpoke.Y2 = rimCenter.Y + R;
+
+            horizontalSpoke.X1 = rimCenter.X - R;
+            horizontalSpoke.Y1 = rimCenter.Y;
+            horizontalSpoke.X2 = rimCenter.X + R;
+            horizontalSpoke.Y2 = rimCenter.Y;
+
+            Canvas.SetLeft(rimPoint, rimCenter.X - 4);
+            Canvas.SetTop(rimPoint, rimCenter.Y - R - 4);
+
+            Canvas.SetLeft(blockPoint, rimCenter.X + rimBlockDistance - 4);
+            Canvas.SetTop(blockPoint, rimCenter.Y - 4);
+
+            joint.X1 = rimCenter.X;
+            joint.Y1 = rimCenter.Y - R;
+            joint.X2 = rimCenter.X + rimBlockDistance;
+            joint.Y2 = rimCenter.Y;
+
+            block.Width = R * 1.5;
+            block.Height = R * 1;
+            Canvas.SetLeft(block, rimCenter.X + rimBlockDistance);
+            Canvas.SetTop(block, rimCenter.Y - block.Height / 2);
+        }
+    }
+}
---
+++ Hodograph/MainWindowInitializer.cs
@@ -0,0 +1,317 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Hodograph
+{
+    public partial class MainWindow
+    {
+        #region Properties
+
+        ObservableCollection<DataPoint> phasePoints;
+        public ObservableCollection<DataPoint> PhasePoints
+        {
+            get { return phasePoints; }
+        }
+
+        ObservableCollection<DataPoint> positionPoints;
+        public ObservableCollection<DataPoint> PositionPoints
+        {
+            get { return positionPoints; }
+        }
+
+        ObservableCollection<DataPoint> currentPhasePoints;
+        public ObservableCollection<DataPoint> CurrentPhasePoints
+        {
+            get { return currentPhasePoints; }
+        }
+
+        ObservableCollection<DataPoint> velocityPoints;
+        public ObservableCollection<DataPoint> VelocityPoints
+        {
+            get { return velocityPoints; }
+        }
+
+        ObservableCollection<DataPoint> accelerationPoints;
+        public ObservableCollection<DataPoint> AccelerationPoints
+        {
+            get { return accelerationPoints; }
+        }
+
+        private double animationSpeed;
+        public double AnimationSpeed
+        {
+            get { return animationSpeed; }
+            set
+            {
+                if (value != animationSpeed)
+                {
+                    animationSpeed = value;
+                    OnPropertyChanged("AnimationSpeed");
+                }
+            }
+        }
+
+        private double epsilon0;
+        public double Epsilon0
+        {
+            get { return epsilon0; }
+            set
+            {
+                if (value != epsilon0)
+                {
+                    epsilon0 = value;
+                    OnPropertyChanged("Epsilon0");
+                }
+            }
+        }
+
+        private double l;
+        public double L
+        {
+            get { return l; }
+            set
+            {
+                if (value != l)
+                {
+                    l = value;
+                    OnPropertyChanged("L");
+                    if (R > L) L = R;
+                    rimBlockDistance = Math.Sqrt(L * L - R * R);
+                    rimCenter = new Point(CanvasHalfWidth - rimBlockDistance * 2 / 3, CanvasHalfHeight);
+                    if(!animationStarted)
+                        SetupSceneObjects();
+                }
+            }
+        }
+
+        private double lCurrent;
+        public double LCurrent
+        {
+            get { return lCurrent; }
+            set
+            {
+                if (value != lCurrent)
+                {
+                    lCurrent = value;
+                    OnPropertyChanged("LCurrent");
+                }
+            }
+        }
+
+        private double r;
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                if (value != r)
+                {
+                    r = value;
+                    OnPropertyChanged("R");
+                    if (R > L) L = R;
+                    rimBlockDistance = Math.Sqrt(L * L - R * R);
+                    rimCenter = new Point(CanvasHalfWidth - rimBlockDistance * 2 / 3, CanvasHalfHeight);
+                    if (!animationStarted)
+                        SetupSceneObjects();
+                }
+            }
+        }
+
+        private double alpha;
+        public double Alpha
+        {
+            get { return alpha; }
+            set
+            {
+                if (value != alpha)
+                {
+                    alpha = value;
+                    OnPropertyChanged("Alpha");
+                }
+            }
+        }
+
+        private double omega;
+        public double Omega
+        {
+            get { return omega; }
+            set
+            {
+                if (value != omega)
+                {
+                    omega = value;
+                    OnPropertyChanged("Omega");
+                }
+            }
+        }
+
+        private double time;
+        public double Time
+        {
+            get { return time; }
+            set
+            {
+                if (value != time)
+                {
+                    time = value;
+                    OnPropertyChanged("Time");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Fields
+
+        Random rnd = new Random(DateTime.Now.Millisecond);
+        System.Windows.Threading.DispatcherTimer dispatcherTimer;
+        private const double Epsilon = 0.0001;
+        private const double CanvasWidth = 700;
+        private const double CanvasHalfWidth = 350;
+        private const double CanvasHeight = 350;
+        private const double CanvasHalfHeight = 175;
+        private bool animationStarted = false;
+        private DateTime startTime;
+        private DateTime stopTime;
+        private TimeSpan timeDelay;
+        private bool[] buttonsFlags;
+        private Ellipse rim;
+        private Line joint;
+        private Point rimCenter;
+        private double rimBlockDistance;
+        private Ellipse rimPoint;
+        private Ellipse blockPoint;
+        private Rectangle block;
+        private Line axisX;
+        private Line axisY;
+        private Line verticalSpoke;
+        private Line horizontalSpoke;
+        private const double StartAlpha = Math.PI / 2;
+        private double currentX;
+        private double nextX;
+        private double nextNextX;
+        private Point positionOnRim;
+        private Point positionOnBlock;
+        private double currentV;
+        private double currentA;
+        private double lastTime;
+        private double deltaTime;
+        private GaussianRandom gaussianRandom;
+        private const int MaxPointsCount = 200;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Window initialization
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Initialize();
+            InitializeScene();
+        }
+
+        private void Initialize()
+        {
+            gaussianRandom = new GaussianRandom(rnd);
+            phasePoints = new ObservableCollection<DataPoint>();
+            currentPhasePoints = new ObservableCollection<DataPoint>();
+            positionPoints = new ObservableCollection<DataPoint>();
+            velocityPoints = new ObservableCollection<DataPoint>();
+            accelerationPoints = new ObservableCollection<DataPoint>();
+            animationSpeed = 0;
+            epsilon0 = 0.05;
+            alpha = StartAlpha;
+            omega = 2;
+            r = 80;
+            lCurrent = l = 300;
+            rimBlockDistance = Math.Sqrt(L * L - R * R);
+            rimCenter = new Point(CanvasHalfWidth - rimBlockDistance * 2 / 3, CanvasHalfHeight);
+
+            currentX = rimBlockDistance;
+            currentV = 0.0;
+            currentA = 0.0;
+            lastTime = 0.0;
+            deltaTime = 0;
+
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimerTick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            buttonsFlags = new bool[3] { false, false, false };
+        }
+
+        private void InitializeScene()
+        {
+            PositionPlot.DefaultColors = new Color[1]{ Colors.DodgerBlue };
+            VelocityPlot.DefaultColors = new Color[1] { Colors.DodgerBlue };
+            AccelerationPlot.DefaultColors = new Color[1] { Colors.DodgerBlue };
+            PhasePlot.DefaultColors = new Color[1] { Colors.DodgerBlue };
+
+
+            axisX = new Line();
+            axisX.StrokeThickness = 1;
+            axisX.Stroke = Brushes.Black;
+            SimulationCanvas.Children.Add(axisX);
+
+            axisY = new Line();
+            axisY.StrokeThickness = 1;
+            axisY.Stroke = Brushes.Black;
+            SimulationCanvas.Children.Add(axisY);
+
+            rim = new Ellipse();
+            rim.Stroke = Brushes.Blue;
+            rim.StrokeThickness = 3;
+            SimulationCanvas.Children.Add(rim);
+
+            verticalSpoke = new Line();
+            verticalSpoke.Stroke = Brushes.Blue;
+            verticalSpoke.StrokeThickness = 2;
+            SimulationCanvas.Children.Add(verticalSpoke);
+
+            horizontalSpoke = new Line();
+            horizontalSpoke.Stroke = Brushes.Blue;
+            horizontalSpoke.StrokeThickness = 2;
+            SimulationCanvas.Children.Add(horizontalSpoke);
+
+            rimPoint = new Ellipse();
+            rimPoint.Width = rimPoint.Height = 8;
+            rimPoint.Fill = Brushes.Blue;
+            SimulationCanvas.Children.Add(rimPoint);
+
+            blockPoint = new Ellipse();
+            blockPoint.Width = blockPoint.Height = 8;
+            blockPoint.Fill = Brushes.Blue;
+            SimulationCanvas.Children.Add(blockPoint);
+
+            joint = new Line();
+            joint.Stroke = Brushes.OrangeRed;
+            joint.StrokeThickness = 3;
+            SimulationCanvas.Children.Add(joint);
+
+            block = new Rectangle();
+            block.Stroke = Brushes.Blue;
+            block.StrokeThickness = 3;
+            SimulationCanvas.Children.Add(block);
+
+            SetupSceneObjects();
+        }
+        #endregion
+    }
+}
